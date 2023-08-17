52bdb049605e554b14025c1f4f1f12705939a6f2
William Lima <williamlima142014@gmail.com>
2023-08-16 22:06:24 -0300

    start

---
+++ src/Display.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function outputCurrency(){
+
+}
+
+export default outputCurrency;
---
+++ src/History.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function inputCurrency(){
+
+}
+
+export default inputCurrency;
